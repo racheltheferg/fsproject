4db6f7dce827fcd3c1ed3c1627d99752e02c0019
Rachel Ferguson <racheltheferg@gmail.com>
2012-10-29 09:16:14 -0400

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+$(function() {
+  $(".pluscover").click(function(){
+    $(".cover").fadeToggle(200);
+  });
+});
+
+$(function() {
+  $(".plusfs1").click(function(){
+    $(".fs").fadeToggle(200);
+  });
+});
+
+$(function() {
+  $(".plusfs2").click(function(){
+    $(".fs2").fadeToggle(200);
+  });
+});
+
+$(function() {
+  $(".plusfs3").click(function(){
+    $(".fs3").fadeToggle(200);
+  });
+});
+
+$(function() {
+  $(".plus89").click(function(){
+    $(".lady1889").fadeToggle(200);
+  });
+});
+
+$(function() {
+  $(".plusicon1").click(function(){
+    $(".icon").fadeToggle(200);
+  });
+});
+
+$(function() {
+  $(".plusicon2").click(function(){
+    $(".icon2").fadeToggle(200);
+  });
+});
+
+$(function() {
+  $(".plusrev").click(function(){
+    $(".revitalization").fadeToggle(200);
+  });
+});
+
+$(function() {
+  $(".plustoday").click(function(){
+    $(".today").fadeToggle(200);
+  });
+});
+
+
